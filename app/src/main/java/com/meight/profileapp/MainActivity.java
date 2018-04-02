525a681a2c83f9458c5a43beb4703039c739d925
Meight <7ace.lex@gmail.com>
2018-04-02 14:34:39 -0600

    colocando todo solo en LinearLayout

--- app/src/main/java/com/meight/profileapp/MainActivity.java
+++ app/src/main/java/com/meight/profileapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.meight.profileapp;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
